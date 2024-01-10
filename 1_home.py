806a8d366ec123346c7a71b4101a220ffc04d9c1
duryeong <duryeong@gmail.com>
2024-01-10 22:17:18 +0900

    Add files via upload

--- 1_home.py
+++ 1_home.py
@@ -1,13 +1,54 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-import talib as ta
+import copy
+
+def ADX(df, period=14):
+    data = copy.deepcopy(df)
+    # True Range 계산
+    data['high-low'] = data['high'] - data['low']
+    data['high-close-Prev'] = abs(data['high'] - data['close'].shift(1))
+    data['low-close-Prev'] = abs(data['low'] - data['close'].shift(1))
+    data['TrueRange'] = data[['high-low', 'high-close-Prev', 'low-close-Prev']].max(axis=1)
+
+    # +DI, -DI 계산
+    data['+DM'] = (data['high'] - data['high'].shift(1)).where((data['high'] - data['high'].shift(1)) > 0, 0)
+    data['-DM'] = (data['low'].shift(1) - data['low']).where((data['low'].shift(1) - data['low']) > 0, 0)
+
+    data['+DI'] = (data['+DM'].rolling(window=period).sum() / data['TrueRange'].rolling(window=period).sum()) * 100
+    data['-DI'] = (data['-DM'].rolling(window=period).sum() / data['TrueRange'].rolling(window=period).sum()) * 100
+
+    # ADX 계산
+    data['DX'] = (abs(data['+DI'] - data['-DI']) / (data['+DI'] + data['-DI'])) * 100
+    data['ADX'] = data['DX'].rolling(window=period).mean()
+    return data['ADX']
+
+def SMA(data, period=30, column='close'):
+    return data[column].rolling(window=period).mean()
+
+def RSI(data, period=14, column='close'):
+    delta = data[column].diff(1)
+    delta = delta.dropna()
+
+    up = delta.copy()
+    down = delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+    data['up'] = up
+    data['down'] = down
+
+    AVG_Gain = SMA(data, period, column='up')
+    AVG_Loss = abs(SMA(data, period, column='down'))
+    RS = AVG_Gain / AVG_Loss
+
+    RSI = 100.0 - (100.0 / (1.0 + RS))
+
+    return RSI
 
 def make_idx(df, r1=7, ad=14, limad=12, wmean=4 ,iyear=None):
-    df[f'rsi{r1}'] = ta.RSI(df['close'], timeperiod=r1)
-    df[f'rsi{r1*2}'] = ta.RSI(df['close'], timeperiod=r1*2)
-    df[f'rsi{r1*3}'] = ta.RSI(df['close'], timeperiod=r1*3)
-    df[f'adx_{ad}'] = ta.ADX(df['high'], df['low'], df['close'], timeperiod=ad)
+    for itime in range(1,4):
+        df[f'rsi{r1*itime}'] = RSI(df, period=r1*itime)
+    df[f'adx_{ad}'] = ADX(df, period=ad)
     df[f'mean{wmean}'] = df.close.rolling(window=wmean).mean()
 
     is_up = []
@@ -39,7 +80,7 @@ def get_stock_info():
 stock_info = get_stock_info()
 st.title(f'HOME')
 st.subheader(f"anal_date:{pd.to_datetime('today').strftime('%Y-%m-%d %H:%M')}")
-tabs = st.tabs([f"itab_{inum+1:03d}" for inum, itab in enumerate(stock_info.tickers.values)])
+tabs = st.tabs([f"{itab}_{inum+1:03d}" for inum, itab in enumerate(stock_info.tickers.values)])
 
 for inum, itab in enumerate(tabs):
     with itab:
