0de58ae280c0975727de56e9623bc62f307017fd
duryeong <duryeong@gmail.com>
2024-01-10 16:34:01 +0900

    Add files via upload
    change 1_home.py

--- 1_home.py
+++ 1_home.py
@@ -1,2 +1,52 @@
 import streamlit as st
-st.title('HOME')
+import pandas as pd
+import yfinance as yf
+import talib as ta
+
+def make_idx(df, r1=7, ad=14, limad=12, wmean=4 ,iyear=None):
+    df[f'rsi{r1}'] = ta.RSI(df['close'], timeperiod=r1)
+    df[f'rsi{r1*2}'] = ta.RSI(df['close'], timeperiod=r1*2)
+    df[f'rsi{r1*3}'] = ta.RSI(df['close'], timeperiod=r1*3)
+    df[f'adx_{ad}'] = ta.ADX(df['high'], df['low'], df['close'], timeperiod=ad)
+    df[f'mean{wmean}'] = df.close.rolling(window=wmean).mean()
+
+    is_up = []
+    for idf in df.iloc:
+        # is_up.append(idf.rsi7 > idf.rsi14 > idf.rsi21 and idf.adx > 20)
+        is_up.append(idf[f'rsi{r1}'] > idf[f'rsi{r1*2}'] > idf[f'rsi{r1*3}'] and idf[f'adx_{ad}'] > limad and idf.close > idf[f'mean{wmean}'])
+    df['is_up'] = is_up
+    df['differ'] = (df.close - df.open)/df.open*100
+    df = df[['open', 'high', 'low', 'close', 'differ', 'is_up']]
+    df = df[::-1]
+    return df
+
+def get_stock(c='AAPL'):
+    stock_code = c
+    # Yahoo Finance에서 주식 정보를 가져옵니다.
+    stock_data = yf.Ticker(stock_code)
+    df = stock_data.history(interval='1d', period='2mo')
+    df.columns = [ic.lower() for ic in list(df.columns)]
+    df = df[['open', 'high', 'low', 'close']]
+    return df
+
+@st.cache_resource
+def get_stock_info():
+    df = pd.read_csv('yfinance_anal.csv')
+    df = df.sort_values(by='best_value', ascending=False)
+    df = df[['tickers', 'best_value', 'best_param']]
+    return df.head(100)
+
+stock_info = get_stock_info()
+st.title(f'HOME')
+st.subheader(f"anal_date:{pd.to_datetime('today').strftime('%Y-%m-%d %H:%M')}")
+tabs = st.tabs([f"itab_{inum+1:03d}" for inum, itab in enumerate(stock_info.tickers.values)])
+
+for inum, itab in enumerate(tabs):
+    with itab:
+        with st.spinner(f'make info {stock_info.tickers.values[inum]}'):
+            # st.write(f'{stock_info.tickers.values[inum]}')
+            st.write(f'{stock_info.best_value.values[inum]*100:.2f}')
+            candle = get_stock(c=stock_info.tickers.values[inum])
+            candle = make_idx(candle)
+            candle
+
