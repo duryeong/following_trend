16ae0540de673fdaa5dbd5351f51daad1a212518
duryeong <duryeong@gmail.com>
2024-01-10 17:18:48 +0900

    Add files via upload

--- 1_home.py
+++ 1_home.py
@@ -1,7 +1,51 @@
+import requests
+import os
+import sys
+import subprocess
+
+def init():
+    # check if the library folder already exists, to avoid building everytime you load the pahe
+    if not os.path.isdir("/tmp/ta-lib"):
+
+        # Download ta-lib to disk
+        with open("/tmp/ta-lib-0.4.0-src.tar.gz", "wb") as file:
+            response = requests.get(
+                "http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz"
+            )
+            file.write(response.content)
+        # get our current dir, to configure it back again. Just house keeping
+        default_cwd = os.getcwd()
+        os.chdir("/tmp")
+        # untar
+        os.system("tar -zxvf ta-lib-0.4.0-src.tar.gz")
+        os.chdir("/tmp/ta-lib")
+        # build
+        os.system("./configure --prefix=/home/appuser/venv/")
+        os.system("make")
+        # install
+        os.system("mkdir -p /home/appuser/venv/")
+        os.system("make install")
+        os.system("ls -la /home/appuser/venv/")
+        # back to the cwd
+        os.chdir(default_cwd)
+        sys.stdout.flush()
+
+# add the library to our current environment
+from ctypes import *
+
+lib = CDLL("/home/appuser/venv/lib/libta_lib.so.0.0.0")
+# import library
+try:
+    import talib
+except ImportError:
+    init()
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--global-option=build_ext", "--global-option=-L/home/appuser/venv/lib/", "--global-option=-I/home/appuser/venv/include/", "ta-lib==0.4.24"])
+finally:
+    import talib
+
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-import talib as ta
 
 def make_idx(df, r1=7, ad=14, limad=12, wmean=4 ,iyear=None):
     df[f'rsi{r1}'] = ta.RSI(df['close'], timeperiod=r1)
