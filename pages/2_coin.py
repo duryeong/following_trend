db9f35e6bd4711a1456fac58f2c90a5a80982334
duryeong <duryeong@gmail.com>
2024-01-15 15:03:58 +0900

    Update 2_coin.py

--- pages/2_coin.py
+++ pages/2_coin.py
@@ -46,7 +46,7 @@ def get_stock_info():
     df = pd.read_csv('coin_anal.csv')
     df = df.sort_values(by='best_value', ascending=False)
     df = df[['tickers', 'best_value', 'best_param']]
-    return df.head(100)
+    return df.head(20)
 
 def web_main():
     stock_info = get_stock_info()
