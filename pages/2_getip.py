3a4729196c656e38fd17ede7af7da6e2a815f023
duryeong <duryeong@gmail.com>
2024-01-15 11:28:11 +0900

    Update 2_getip.py

--- pages/2_getip.py
+++ pages/2_getip.py
@@ -2,6 +2,7 @@ import streamlit as st
 from requests import get
 
 p = st.text_input('passwd')
+st.write(p)
 ip = get('https://api.ipify.org').content.decode('utf8')
 if p == '0240':
     st.subheader('My public IP address is: {}'.format(ip))
