cecf54d9807eda66dd3f6b469b2c19c5d7a5e97d
duryeong <duryeong@gmail.com>
2024-01-16 23:50:20 +0900

    Update 1_stock.py

--- 1_stock.py
+++ 1_stock.py
@@ -37,7 +37,7 @@ def get_stock_info():
     df = pd.read_csv('yfinance_anal.csv')
     df = df.sort_values(by='best_value', ascending=False)
     df = df[['tickers', 'best_value', 'best_param']]
-    return df.head(20)
+    return df.head(30)
 
 def web_main():
     stock_info = get_stock_info()
